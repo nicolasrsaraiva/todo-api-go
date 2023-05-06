c931de8b0eff8e0189b5523299553ae4b2a99cb9
Nícolas Saraiva <132316573+nicolasrsaraiva@users.noreply.github.com>
2023-05-06 09:50:36 -0300

    feat: added new functions to create and show the todos

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 )
 
 func main() {
-
 	todosList := []models.Todo{}
-	models.CreateTodo("", todosList)
+	models.CreateTodo("Estudar java", true, &todosList)
+	models.CreateTodo("Passear com a cachorra", true, &todosList)
+	models.CreateTodo("Ir para a academia", false, &todosList)
+	models.CreateTodo("Almoçar bem", false, &todosList)
+	models.CreateTodo("Comprar um tênis", false, &todosList)
+
+	models.ShowTodos(todosList)
+	models.ShowCompletedTodos(todosList)
+	models.ShowIncompletedTodos(todosList)
+
 }
--- models/todo.go
+++ models/todo.go
@@ -1,12 +1,47 @@
 package models
 
+import "fmt"
+
 type Todo struct {
-	id          int
-	description string
-	done        bool
+	Id          int
+	Description string
+	Done        bool
 }
 
-func CreateTodo(desc string, todosList []Todo) {
-	todo := Todo{description: desc, done: false}
-	todosList = append(todosList, todo)
+func CreateTodo(desc string, done bool, todosList *[]Todo) {
+	todo := Todo{Description: desc, Done: done}
+	*todosList = append(*todosList, todo)
+}
+
+func ShowTodos(todosList []Todo) {
+	fmt.Println("All ToDos:")
+	for _, todo := range todosList {
+		fmt.Printf("Description: %s | Done: %t\n", todo.Description, todo.Done)
+	}
+}
+
+func ShowCompletedTodos(todosList []Todo) {
+	fmt.Println("All completed ToDos:")
+	for _, todo := range todosList {
+		if todo.Done {
+			fmt.Printf("Description: %s | Done: %t\n", todo.Description, todo.Done)
+		}
+	}
+}
+
+func ShowIncompletedTodos(todosList []Todo) {
+	fmt.Println("All incompleted ToDos:")
+	for _, todo := range todosList {
+		if !todo.Done {
+			fmt.Printf("Description: %s | Done: %t\n", todo.Description, todo.Done)
+		}
+	}
+}
+
+func TodoCompleted(todoCompleted *Todo, todosList *[]Todo) {
+	for _, todo := range *todosList {
+		if todo.Description == todoCompleted.Description {
+			todo.Done = true
+		}
+	}
 }
